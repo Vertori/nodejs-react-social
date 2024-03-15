c6e36da8dbaf4ee2801e0f9209959fb1ce8ffb75
Vertori <115083330+Vertori@users.noreply.github.com>
2024-03-15 17:45:48 +0100

    error middleware

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,26 +1,28 @@
-const getContacts = (req, res) => {
-  res.status(200).json({ message: "Fetched contacts!" });
-};
+const asyncHandler = require("express-async-handler");
 
-const createContact = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Fetched contacts!" });
+});
+
+const createContact = asyncHandler(async (req, res) => {
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fields are required!");
   }
   res.status(201).json({ message: "Added new contact!" });
-};
+});
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res
     .status(201)
     .json({ message: `Modified contact with id: ${req.params.id}` });
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res
     .status(201)
     .json({ message: `Deleted contact with id: ${req.params.id}` });
-};
+});
 
 module.exports = { getContacts, createContact, updateContact, deleteContact };
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const contactRoutes = require("./routes/contactRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/contacts", contactRoutes);
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
