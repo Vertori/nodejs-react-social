901199aebaad7a6cbdeeed1dc21641c6a880a67c
Vertori <115083330+Vertori@users.noreply.github.com>
2024-03-16 00:48:10 +0100

    register user functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,32 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required!");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered!");
+  }
+  //hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log(`Usere created: ${user}`);
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
   res.json({ message: "User registered" });
 });
 
