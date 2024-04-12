ab140f7e92511a0591a36f30c1f1f5573e9e4d48
Vertori <115083330+Vertori@users.noreply.github.com>
2024-04-12 23:00:43 +0200

    display fetched search results

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { recipesCategories } from "../lib/constants";
+import PublicRecipe from "../components/PublicRecipe";
+import { UserRecipe } from "../types";
 
 const SearchPage = () => {
   const [searchFilterData, setSearchFilterData] = useState({
@@ -9,7 +11,7 @@ const SearchPage = () => {
     sort: "desc",
     category: "",
   });
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState<UserRecipe[]>([]);
   const [loading, setLoading] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
@@ -17,7 +19,7 @@ const SearchPage = () => {
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get("searchTerm") || "";
-    const sortFromUrl = urlParams.get("sort") || "";
+    const sortFromUrl = urlParams.get("sort") || "desc";
     const categoryFromUrl = urlParams.get("category") || "";
     if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
       setSearchFilterData({
@@ -74,7 +76,9 @@ const SearchPage = () => {
     e.preventDefault();
     const urlParams = new URLSearchParams(location.search);
     urlParams.set("searchTerm", searchFilterData.searchTerm);
-    urlParams.set("category", searchFilterData.category);
+    if (searchFilterData.category) {
+      urlParams.set("category", searchFilterData.category);
+    }
     urlParams.set("sort", searchFilterData.sort);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
@@ -115,7 +119,9 @@ const SearchPage = () => {
             className="w-full max-w-xs select select-bordered"
             id="category"
             onChange={handleInputChange}
+            value={searchFilterData.category || ""}
           >
+            <option disabled selected></option>
             {recipesCategories.map((category, index) => (
               <option key={index} value={category.name}>
                 {category.name}
@@ -141,10 +147,30 @@ const SearchPage = () => {
             <option value="asc">Oldest</option>
           </select>
         </div>
-        <button type="submit" className="btn">
+        <button type="submit" className="md:self-end btn">
           Filter
         </button>
       </form>
+      <div className="w-full mt-12 ">
+        <h1 className="pb-2 text-xl font-medium border-b border-gray-300 font-primary">
+          Searched results
+        </h1>
+        <div className="grid grid-cols-1 gap-3 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+          {!loading && recipes.length === 0 && (
+            <p className="text-xl text-gray-500 font-primary">
+              No recipes found.
+            </p>
+          )}
+          {loading && (
+            <span className="loading loading-spinner loading-md"></span>
+          )}
+          {!loading &&
+            recipes &&
+            recipes.map((recipe) => (
+              <PublicRecipe recipe={recipe} key={recipe._id} />
+            ))}
+        </div>
+      </div>
     </section>
   );
 };
